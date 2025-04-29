8bbe57d2aa7680b1102a5f4433e42d59ad6ea567
Muriel-Boschen <murielboschen1234@gmail.com>
2025-04-29 16:22:43 -0300

    Update scraper_wikipedia.py

--- scraper_wikipedia.py
+++ scraper_wikipedia.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 from openpyxl import Workbook
 from datetime import datetime
 
+
 url = 'https://pt.wikipedia.org/wiki/Portal:Eventos_atuais'
+
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
@@ -17,9 +19,12 @@ ws.append(["Manchete", "Data"])
 
 data_hoje = datetime.today().strftime('%Y-%m-%d')
 
+
 for item in eventos:
-    texto = item.get_text()
-    ws.append([texto, data_hoje])
+    texto = item.get_text().strip()  
+    if texto: 
+        ws.append([texto, data_hoje])
+
 
 wb.save("eventos_atuais.xlsx")
 print("Planilha criada com sucesso!")
