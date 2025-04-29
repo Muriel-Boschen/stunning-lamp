ba14f72abef648ce7ec85c16a40cea861ccda496
Muriel-Boschen <murielboschen1234@gmail.com>
2025-04-29 15:36:41 -0300

    Update scraper_wikipedia.py
    Adiciona requisição HTTP e parser HTML

--- scraper_wikipedia.py
+++ scraper_wikipedia.py
@@ -2,3 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
 from datetime import datetime
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
